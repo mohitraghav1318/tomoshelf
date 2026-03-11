f576d53cb90c62726cefb410ccfa3920c9f5f406
mohitraghav1318 <mohitraghav1318@gmail.com>
2026-03-11 19:15:19 +0530

    fix

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "http://localhost:5000/api || https://tomoshelf.onrender.com"
+    baseURL: import.meta.env.VITE_API_URL
 });
 
 export default API;
